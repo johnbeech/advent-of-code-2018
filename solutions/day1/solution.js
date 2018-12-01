10ff576e130e56a7d0b644ecb6fca6e0ab75454d
John Beech <johnbeech.github@mkv25.net>
2018-12-01 08:58:06 +0000

    Create empty day1 solution

---
+++ solutions/day1/solution.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const { read } = require('promise-path')
+
+async function run () {
+  const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
+  let solution = 'UNSOLVED'
+
+  report(input, solution)
+}
+
+function report (input, solution) {
+  const solutionName = __dirname.split(path.sep).pop()
+  console.log('Advent of Code 2017 :', solutionName, 'solution for', input, ':', solution)
+}
+
+run()
