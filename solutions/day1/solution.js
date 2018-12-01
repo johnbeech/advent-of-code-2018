1c5beedf7e064c4025e754f5e60e91925ecc3f38
John Beech <johnbeech.github@mkv25.net>
2018-12-01 15:44:14 +0000

    Solve day1 for first star.

--- solutions/day1/solution.js
+++ solutions/day1/solution.js
@@ -3,14 +3,16 @@ const { read } = require('promise-path')
 
 async function run () {
   const input = await read(path.join(__dirname, 'input.txt'), 'utf8')
-  let solution = 'UNSOLVED'
+  const frequencies = input.split('\n').filter(n => n).map(n => Number.parseInt(n))
+
+  const solution = frequencies.reduce((acc, n) => acc + n, 0)
 
   report(input, solution)
 }
 
 function report (input, solution) {
   const solutionName = __dirname.split(path.sep).pop()
-  console.log('Advent of Code 2017 :', solutionName, 'solution for', input, ':', solution)
+  console.log('Advent of Code 2018 :', solutionName, 'solution for', input, ':', solution)
 }
 
 run()
