8f3e5636aeaf40edb2b3dac45f55ac04659678c5
John Beech <johnbeech.github@mkv25.net>
2018-12-01 16:56:22 +0000

    Completely over-engineer the copy-template script using javascript

--- copy-template.sh
+++
@@ -1,9 +0,0 @@
-DIRECTORY="solutions/$1"
-TEMPLATE="solutions/template"
-
-if [ ! -d "$DIRECTORY" ]; then
-  echo "Creating $DIRECTORY from template: $TEMPLATE"
-  cp -r $TEMPLATE $DIRECTORY
-else
-  echo "Path $DIRECTORY already exists, doing nothing."
-fi
