cd9d9c64978d4f592928ef1d2ccc9e3681237527
John Beech <johnbeech.github@mkv25.net>
2018-12-01 17:00:21 +0000

    Add error case when no argument is passed to run.js

--- run.js
+++ run.js
@@ -2,5 +2,9 @@ const solutionId = process.argv[2]
 try {
   require(`./solutions/${solutionId}/solution.js`)
 } catch (ex) {
-  console.error(`Unable to run solution for '${solutionId}': ${ex}`, ex.stack)
+  if (!solutionId) {
+    console.error('No solution ID provided; please re-run with an argument, e.g.: npm start day1, or: node run day1')
+  } else {
+    console.error(`Unable to run solution for '${solutionId}': ${ex}`, ex.stack)
+  }
 }
