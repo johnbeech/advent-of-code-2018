eba677aad740a80ae4f45f59c01076f4352229f1
John Beech <johnbeech.github@mkv25.net>
2018-12-03 12:46:09 +0000

    Parse claim lines into keyed objects

--- solutions/day3/solution.js
+++ solutions/day3/solution.js
@@ -5,9 +5,28 @@ const report = (...messages) => console.log(`[${require(fromHere('../../package.
 
 async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+  const claims = parseClaims(input)
 
-  await solveForFirstStar(input)
-  await solveForSecondStar(input)
+  await solveForFirstStar(claims)
+  await solveForSecondStar(claims)
+}
+
+function parseClaims (input) {
+  const lines = input.split('\n').filter(n => n)
+  return lines.map(parseClaimLine)
+}
+
+// Example claim line: #1 @ 287,428: 27x20
+const claimLineRegex = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
+function parseClaimLine (line) {
+  const parts = line.match(claimLineRegex)
+  return {
+    claimId: parts[1],
+    left: parts[2],
+    top: parts[3],
+    width: parts[4],
+    height: parts[5]
+  }
 }
 
 async function solveForFirstStar (input) {
