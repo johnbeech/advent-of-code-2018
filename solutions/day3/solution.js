4506c5e4bf08611da6bafcab78fd615f2a7d1df5
John Beech <johnbeech.github@mkv25.net>
2018-12-03 13:17:16 +0000

    Solve day3 for first star.

--- solutions/day3/solution.js
+++ solutions/day3/solution.js
@@ -22,17 +22,42 @@ function parseClaimLine (line) {
   const parts = line.match(claimLineRegex)
   return {
     claimId: parts[1],
-    left: parts[2],
-    top: parts[3],
-    width: parts[4],
-    height: parts[5]
+    left: parseInt(parts[2]),
+    top: parseInt(parts[3]),
+    width: parseInt(parts[4]),
+    height: parseInt(parts[5])
   }
 }
 
-async function solveForFirstStar (input) {
-  let solution = 'UNSOLVED'
-  report('Input:', input)
+async function solveForFirstStar (claims) {
+  report('Claims:', claims)
+
+  const claimedTiles = claims.reduce(claimArea, {})
+
+  report('Claimed tiles:', claimedTiles)
+
+  const solution = Object.entries(claimedTiles).reduce(countOverlappingClaims, 0)
+
   report('Solution 1:', solution)
+}
+
+function claimArea (area, coords) {
+  let key
+  for (let y = 0; y < coords.height; y++) {
+    for (let x = 0; x < coords.width; x++) {
+      key = (coords.left + x) + ',' + (coords.top + y)
+      area[key] = area[key] || 0
+      area[key]++
+    }
+  }
+  return area
+}
+
+function countOverlappingClaims (acc, kvp) {
+  if (kvp[1] > 1) {
+    return acc + 1
+  }
+  return acc
 }
 
 async function solveForSecondStar (input) {
