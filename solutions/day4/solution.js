3649b5f6d8f3cd146c72b93a27a5580eb9fa9225
John Beech <johnbeech.github@mkv25.net>
2018-12-04 11:41:01 +0000

    Map event lines to objects using regex

--- solutions/day4/solution.js
+++ solutions/day4/solution.js
@@ -6,8 +6,33 @@ const report = (...messages) => console.log(`[${require(fromHere('../../package.
 async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8')).trim()
 
-  await solveForFirstStar(input)
-  await solveForSecondStar(input)
+  const events = input.split('\n').filter(n => n).map(parseEventLine)
+
+  await solveForFirstStar(events)
+  await solveForSecondStar(events)
+}
+
+// [1518-09-05 00:51] wakes up
+// [1518-05-29 00:00] Guard #2713 begins shift
+// [1518-10-21 00:40] falls asleep
+const eventLineRegex = /\[(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2})] (.*)/
+const guardBeginsShiftRegex = /Guard #(\d+) begins shift/
+function parseEventLine (line) {
+  const parts = line.match(eventLineRegex)
+  const event = {
+    date: parts[1],
+    time: parts[2],
+    message: parts[3].trim()
+  }
+
+  event.timeStamp = (new Date(event.date + ' ' + event.time)).getTime()
+  event.wakeUp = event.message === 'wakes up'
+  event.fallsAsleep = event.message === 'falls asleep'
+  if (guardBeginsShiftRegex.test(event.message)) {
+    event.beginsShift = true
+    event.guardId = event.message.match(guardBeginsShiftRegex)[1]
+  }
+  return event
 }
 
 async function solveForFirstStar (input) {
