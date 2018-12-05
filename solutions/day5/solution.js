9dbb8a3b9a6d43d282677fcceaf57ce8d320a201
John Beech <john.beech@sorted.com>
2018-12-05 11:40:24 +0000

    Solve day 5 for first star.

--- solutions/day5/solution.js
+++ solutions/day5/solution.js
@@ -11,8 +11,35 @@ async function run () {
 }
 
 async function solveForFirstStar (input) {
-  let solution = 'UNSOLVED'
-  report('Input:', input)
+
+  let polymer = input.split('')
+
+  function reactPolymer(list) {
+    let filterNext = false
+    const startLength = list.length
+    polymer = list.filter((char, index) => {
+      if (filterNext) {
+        filterNext = false
+        return false
+      }
+      else {
+        let next = list[index + 1] || ''
+        if (char !== next && char.toLowerCase() === next.toLowerCase()) {
+          filterNext = true
+          return false
+        }
+      }
+      return true
+    })
+    return (polymer.length !== startLength)
+  }
+
+  do {
+     report('Polymer length:', polymer.length)
+  } while (reactPolymer(polymer))
+
+  let solution = polymer.length
+  report('Final polymer:', polymer.join(''))
   report('Solution 1:', solution)
 }
 
