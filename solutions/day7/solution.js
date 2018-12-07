f78f3723dfa507f0ae5821aedc90bd7a99b32873
John Beech <john.beech@sorted.com>
2018-12-07 14:32:32 +0000

    Parse useful data out of input; haven't read the problem yet.

--- solutions/day7/solution.js
+++ solutions/day7/solution.js
@@ -6,13 +6,25 @@ const report = (...messages) => console.log(`[${require(fromHere('../../package.
 async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8')).trim()
 
-  await solveForFirstStar(input)
+  const dependencies = input.split('\n').filter(n => n).map(parseInstruction)
+
+  await solveForFirstStar(dependencies)
   await solveForSecondStar(input)
 }
 
-async function solveForFirstStar (input) {
+// Step M must be finished before step Z can begin.
+const instructionLineRegex = /Step ([A-Z]) must be finished before step ([A-Z]) can begin\./
+function parseInstruction (line) {
+  const matches = line.match(instructionLineRegex)
+  return {
+    step: matches[1],
+    dependsOn: matches[2]
+  }
+}
+
+async function solveForFirstStar (dependencies) {
   let solution = 'UNSOLVED'
-  report('Input:', input)
+  report('Dependencies:', dependencies)
   report('Solution 1:', solution)
 }
 
