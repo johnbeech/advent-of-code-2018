9afb22f9e733ae22549797044fec8e7e83e5ab91
John Beech <johnbeech.github@mkv25.net>
2018-12-07 20:26:15 +0000

    Solve day 7 for first star.

--- solutions/day7/solution.js
+++ solutions/day7/solution.js
@@ -6,9 +6,9 @@ const report = (...messages) => console.log(`[${require(fromHere('../../package.
 async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8')).trim()
 
-  const dependencies = input.split('\n').filter(n => n).map(parseInstruction)
+  const instructions = input.split('\n').filter(n => n).map(parseInstruction)
 
-  await solveForFirstStar(dependencies)
+  await solveForFirstStar(instructions)
   await solveForSecondStar(input)
 }
 
@@ -18,38 +18,74 @@ function parseInstruction (line) {
   const matches = line.match(instructionLineRegex)
   return {
     step: matches[1],
-    dependsOn: matches[2]
+    prevents: matches[2]
   }
 }
 
-async function solveForFirstStar (dependencies) {
-  const dependencyIndex = dependencies.reduce((acc, item) => {
-    acc[item.step] = item
-    item.requiredSteps = []
+async function solveForFirstStar (instructions) {
+  const dependencyIndex = instructions.reduce((acc, item) => {
+    const indexItem = acc[item.step] || {
+      step: item.step,
+      prevents: [],
+      dependsOn: []
+    }
+    indexItem.prevents.push(item.prevents)
+    acc[item.step] = indexItem
     return acc
   }, {})
 
-  dependencies.forEach(item => {
-    let parent = dependencyIndex[item.dependsOn]
-    if (!parent) {
-      dependencyIndex[item.dependsOn] = {
-        step: item.dependsOn,
-        dependsOn: false,
-        requiredSteps: []
+  Object.values(dependencyIndex).forEach(item => {
+    item.prevents.forEach(k => {
+      const indexItem = dependencyIndex[k] || {
+        step: k,
+        prevents: [],
+        dependsOn: []
       }
-      parent = dependencyIndex[item.dependsOn]
-      report('Found start node:', parent)
-    }
-    parent.requiredSteps.push(item.step)
-    parent.requiredSteps = parent.requiredSteps.sort()
+      indexItem.dependsOn.push(item.step)
+      dependencyIndex[k] = indexItem
+    })
   })
 
-  await write(fromHere('dependencies.json'), JSON.stringify({ dependencies }, null, 2), 'utf8')
+  await write(fromHere('dependencies.json'), JSON.stringify({ dependencies: Object.values(dependencyIndex) }, null, 2), 'utf8')
 
-  let solution = 'UNSOLVED'
+  let solution = ''
 
-  report('Dependencies:', dependencies)
+  while (workOnItems()) {
+    report('Solution progress:', solution)
+  }
+
+  function workOnItems() {
+    const startSolution = solution
+    const readyToStartItems = Object.values(dependencyIndex)
+      .filter(n => !n.complete && n.dependsOn.length === 0)
+      .sort(sortOnStep)
+    if (readyToStartItems.length) {
+      workOnItem(readyToStartItems[0])
+    }
+    report('Ready to start', readyToStartItems)
+    return (startSolution !== solution)
+  }
+
+  function workOnItem(item) {
+    solution += item.step
+    item.complete = true
+    item.prevents.forEach(k => {
+      dependencyIndex[k].dependsOn = dependencyIndex[k].dependsOn.filter(n => n !== item.step)
+    })
+  }
+
+  report('Dependencies:', dependencyIndex)
   report('Solution 1:', solution)
+}
+
+function sortOnStep(a, b) {
+  if (a.step < b.step) {
+    return -1
+  }
+  if (a.step > b.step) {
+    return 1
+  }
+  return 0
 }
 
 async function solveForSecondStar (input) {
