2e22ed10678f7d53d36ed58323ccc1099649b6df
John Beech <johnbeech.github@mkv25.net>
2018-12-08 23:39:45 +0000

    Solve day 8 for second star

--- solutions/day8/solution.js
+++ solutions/day8/solution.js
@@ -7,11 +7,10 @@ async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8')).trim()
   const numbers = input.split(' ').filter(n => n).map(n => Number.parseInt(n))
 
-  await solveForFirstStar(numbers)
-  await solveForSecondStar(numbers)
+  await solveForFirstAndSecondStar(numbers)
 }
 
-async function solveForFirstStar (numbers) {
+async function solveForFirstAndSecondStar (numbers) {
   let metaDataTotal = 0
 
   let i = -1
@@ -39,13 +38,25 @@ async function solveForFirstStar (numbers) {
       node.metaData.push(metaDataNum)
     }
 
+    if (children.length) {
+      node.value = node.metaData.reduce((a, b) => {
+        const child = children[b - 1]
+        if (child) {
+          return a + child.value
+        }
+        return a
+      }, 0)
+    } else {
+      node.value = node.metaData.reduce((a, b) => a + b, 0)
+    }
+
     nodes.push(node)
 
     return node
   }
 
   const nodes = []
-  readNode(numbers, nodes)
+  const startNode = readNode(numbers, nodes)
 
   report('Writing nodes', nodes.length)
   await write(fromHere('nodes.json'), JSON.stringify({ nodes }, null, 2), 'utf8')
@@ -53,11 +64,7 @@ async function solveForFirstStar (numbers) {
   const solution = metaDataTotal
 
   report('Solution 1:', solution)
-}
-
-async function solveForSecondStar (input) {
-  let solution = 'UNSOLVED'
-  report('Solution 2:', solution)
+  report('Solution 2:', startNode.value)
 }
 
 run()
