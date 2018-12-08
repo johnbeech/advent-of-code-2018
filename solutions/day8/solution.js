ee7acda379d417ffab5aa23c4dbc08026b1c816e
John Beech <johnbeech.github@mkv25.net>
2018-12-08 23:21:22 +0000

    Solve day 8 for first star.

--- solutions/day8/solution.js
+++ solutions/day8/solution.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { read, position } = require('promise-path')
+const { read, write, position } = require('promise-path')
 const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
@@ -12,8 +12,46 @@ async function run () {
 }
 
 async function solveForFirstStar (numbers) {
-  let solution = 'UNSOLVED'
-  report('Numbers:', numbers)
+  let metaDataTotal = 0
+
+  let i = -1
+  function nextNumber () {
+    i++
+    return numbers[i]
+  }
+
+  function readNode (numbers, nodes) {
+    const node = {
+      start: i,
+      metaData: []
+    }
+    const children = []
+    const numChildren = nextNumber()
+    const numMetaData = nextNumber()
+
+    while (children.length < numChildren) {
+      children.push(readNode(numbers, nodes))
+    }
+
+    while (node.metaData.length < numMetaData) {
+      let metaDataNum = nextNumber()
+      metaDataTotal += metaDataNum
+      node.metaData.push(metaDataNum)
+    }
+
+    nodes.push(node)
+
+    return node
+  }
+
+  const nodes = []
+  readNode(numbers, nodes)
+
+  report('Writing nodes', nodes.length)
+  await write(fromHere('nodes.json'), JSON.stringify({ nodes }, null, 2), 'utf8')
+
+  const solution = metaDataTotal
+
   report('Solution 1:', solution)
 }
 
