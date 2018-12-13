f5e70a1df6ab0785ab66897ca53ff80c9e7e60fb
John Beech <johnbeech.github@mkv25.net>
2018-12-13 17:14:57 +0000

    Parse railway to JSON structure

--- solutions/day13/solution.js
+++ solutions/day13/solution.js
@@ -1,13 +1,53 @@
 const path = require('path')
-const { read, position } = require('promise-path')
+const { read, write, position } = require('promise-path')
 const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
 async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8'))
 
+  const railway = parseRailway(input)
+
+  await write(fromHere('railyway.json'), JSON.stringify(railway, null, 2), 'utf8')
+
   await solveForFirstStar(input)
   await solveForSecondStar(input)
+}
+
+const cartToTrackMap = {
+  '>': '-',
+  '<': '-',
+  '^': '|',
+  'v': '|'
+}
+
+function isCart (symbol) {
+  return !!cartToTrackMap[symbol]
+}
+
+function parseRailway (input) {
+  const grid = input.split('\n').map(line => line.split(''))
+
+  const carts = []
+  grid.forEach((row, j) => {
+    row.forEach((cell, i) => {
+      if (isCart(cell)) {
+        row[i] = cartToTrackMap[cell]
+        carts.push({
+          cartId: carts.length,
+          x: i,
+          y: j,
+          symbol: cell
+        })
+      }
+    })
+  })
+
+  return {
+    grid,
+    carts,
+    collisions: []
+  }
 }
 
 async function solveForFirstStar (input) {
