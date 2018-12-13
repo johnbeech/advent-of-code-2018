1c4a1c1f566351dec7c6d1999c208fed96f99d00
John Beech <johnbeech.github@mkv25.net>
2018-12-13 18:25:11 +0000

    Solve day 13 for first star.

--- solutions/day13/solution.js
+++ solutions/day13/solution.js
@@ -10,7 +10,7 @@ async function run () {
 
   await write(fromHere('railyway.json'), JSON.stringify(railway, null, 2), 'utf8')
 
-  await solveForFirstStar(input)
+  await solveForFirstStar(railway)
   await solveForSecondStar(input)
 }
 
@@ -21,8 +21,73 @@ const cartToTrackMap = {
   'v': '|'
 }
 
+const cartDirectionMap = {
+  '>': { x: 1, y: 0 },
+  '<': { x: -1, y: 0 },
+  '^': { x: 0, y: -1 },
+  'v': { x: 0, y: 1 }
+}
+
+const cartDirectionChangeMap = {
+  '^/': '>',
+  '>/': '^',
+  'v/': '<',
+  '</': 'v',
+  '^\\': '<',
+  '>\\': 'v',
+  'v\\': '>',
+  '<\\': '^',
+  '^+': '?',
+  '>+': '?',
+  'v+': '?',
+  '<+': '?'
+}
+
+const cartTurnOrderMap = {
+  '^': ['<', '^', '>'],
+  '>': ['^', '>', 'v'],
+  'v': ['>', 'v', '<'],
+  '<': ['v', '<', '^']
+}
+
 function isCart (symbol) {
   return !!cartToTrackMap[symbol]
+}
+
+function moveCart (cart, railway) {
+  const nx = cart.x + cartDirectionMap[cart.symbol].x
+  const ny = cart.y + cartDirectionMap[cart.symbol].y
+
+  const newRail = railway.grid[ny][nx]
+  let newSymbol = cartDirectionChangeMap[cart.symbol + newRail] || cart.symbol
+  if (newSymbol === '?') {
+    newSymbol = cartTurnOrderMap[cart.symbol][cart.turnCount % 3]
+    cart.turnCount++
+  }
+  cart.symbol = newSymbol
+  cart.x = nx
+  cart.y = ny
+
+  checkForCollisions(cart, railway)
+}
+
+function checkForCollisions (cart, railway) {
+  railway.carts.reduce((acc, otherCart) => {
+    if (cart === otherCart) {
+      return acc || false
+    }
+    const collided = (cart.x === otherCart.x && cart.y === otherCart.y)
+    if (collided) {
+      cart.symbol = 'X'
+      otherCart.symbol = 'X'
+      railway.collisions.push({
+        x: cart.x,
+        y: cart.y,
+        carts: [cart, otherCart]
+      })
+    }
+    return acc || collided
+  }, false)
 }
 
 function parseRailway (input) {
@@ -37,7 +102,8 @@ function parseRailway (input) {
           cartId: carts.length,
           x: i,
           y: j,
-          symbol: cell
+          symbol: cell,
+          turnCount: 0
         })
       }
     })
@@ -50,10 +116,16 @@ function parseRailway (input) {
   }
 }
 
-async function solveForFirstStar (input) {
-  let solution = 'UNSOLVED'
-  report('Input:', input)
-  report('Solution 1:', solution)
+async function solveForFirstStar (railway) {
+  let step = 0
+  while (railway.collisions.length === 0) {
+    moveCart(railway.carts[step % railway.carts.length], railway)
+    step++
+  }
+
+  let solution = railway.collisions[0]
+
+  report('Solution 1:', solution, `${solution.x},${solution.y}`)
 }
 
 async function solveForSecondStar (input) {
