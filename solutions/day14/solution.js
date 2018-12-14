3ce6169c4d6a67c43ce83be53fac5b61d43a3222
John Beech <johnbeech.github@mkv25.net>
2018-12-14 17:20:23 +0000

    Solve day 14 for second star

--- solutions/day14/solution.js
+++ solutions/day14/solution.js
@@ -27,16 +27,38 @@ async function solveForFirstStar (input) {
   report('Solution 1:', solution)
 }
 
-function populateScoreboard (elves, scoreboard) {
+function populateScoreboard (elves, scoreboard, input) {
   const total = scoreboard[elves[0].recipeIndex] + scoreboard[elves[1].recipeIndex]
   const digits = (total + '').split('').map(n => Number.parseInt(n))
   scoreboard.push(...digits)
   elves[0].recipeIndex = (1 + elves[0].recipeIndex + scoreboard[elves[0].recipeIndex]) % scoreboard.length
   elves[1].recipeIndex = (1 + elves[1].recipeIndex + scoreboard[elves[1].recipeIndex]) % scoreboard.length
+
+  if (input) {
+    const searchRange = scoreboard.slice(-20)
+    const searchFind = searchRange.join('').indexOf(input + '')
+    if (searchFind !== -1) {
+      report(input, 'found in', searchRange, 'at', searchFind)
+      return (scoreboard.length - 20) + searchFind
+    }
+  }
+  return false
 }
 
 async function solveForSecondStar (input) {
-  let solution = 'UNSOLVED'
+  const elves = [{ recipeIndex: 0 }, { recipeIndex: 1 }]
+  const scoreboard = [3, 7]
+  let iteration = 0
+  let patternFound = false
+  while (patternFound === false) {
+    patternFound = populateScoreboard(elves, scoreboard, input)
+    if (iteration % 10000 === 0) {
+      report('Searching:', iteration)
+    }
+    iteration++
+  }
+
+  let solution = patternFound
   report('Solution 2:', solution)
 }
 
