97064345670f26dd4656676a1c8d7c0d9d71554f
John Beech <johnbeech.github@mkv25.net>
2018-12-14 17:05:40 +0000

    Solve day 14 for first star.

--- solutions/day14/solution.js
+++ solutions/day14/solution.js
@@ -11,9 +11,28 @@ async function run () {
 }
 
 async function solveForFirstStar (input) {
-  let solution = 'UNSOLVED'
+  const elves = [{ recipeIndex: 0 }, { recipeIndex: 1 }]
+  const scoreboard = [3, 7]
+  let iteration = 0
+  while (scoreboard.length < input + 10) {
+    populateScoreboard(elves, scoreboard)
+    if (scoreboard.length < 20) {
+      report(iteration, ':', scoreboard.join(' '))
+    }
+  }
+
+  const solution = scoreboard.slice(input, input + 10).join('')
+
   report('Input:', input)
   report('Solution 1:', solution)
+}
+
+function populateScoreboard (elves, scoreboard) {
+  const total = scoreboard[elves[0].recipeIndex] + scoreboard[elves[1].recipeIndex]
+  const digits = (total + '').split('').map(n => Number.parseInt(n))
+  scoreboard.push(...digits)
+  elves[0].recipeIndex = (1 + elves[0].recipeIndex + scoreboard[elves[0].recipeIndex]) % scoreboard.length
+  elves[1].recipeIndex = (1 + elves[1].recipeIndex + scoreboard[elves[1].recipeIndex]) % scoreboard.length
 }
 
 async function solveForSecondStar (input) {
